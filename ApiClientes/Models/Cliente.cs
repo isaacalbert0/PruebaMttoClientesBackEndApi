8ddedeeda4faa5ead6feb4d9a97382a1c2e7b8b8
isaac arevalo <isaac.alberto.ap@gmail.com>
2023-03-18 17:45:31 -0600

    Back End API

---
+++ ApiClientes/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiClientes.Models;
+
+public partial class Cliente
+{
+    public string Identificacion { get; set; } = null!;
+
+    public string? PrimerNombre { get; set; }
+
+    public string? PrimerApellido { get; set; }
+
+    public int? Edad { get; set; }
+
+    public DateTime? FechaCreacion { get; set; }
+
+    public byte[]? Fotografia { get; set; }
+}
---
+++ ApiClientes/Models/MantenimientoClientesContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiClientes.Models;
+
+public partial class MantenimientoClientesContext : DbContext
+{
+    public MantenimientoClientesContext()
+    {
+    }
+
+    public MantenimientoClientesContext(DbContextOptions<MantenimientoClientesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Identificacion);
+
+            entity.Property(e => e.Identificacion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaCreacion)
+                .HasColumnType("datetime")
+                .HasColumnName("Fecha_Creacion");
+            entity.Property(e => e.PrimerApellido)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("Primer_Apellido");
+            entity.Property(e => e.PrimerNombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("Primer_Nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
